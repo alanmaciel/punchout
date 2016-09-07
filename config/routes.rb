dc25c8a2b31747833cfbbe5c31c454fdc8a85b1b
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-09-07 10:44:36 -0500

    Added Admin Menu, and Late entries report

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new', as: 'login'
   delete 'logout' => 'sessions#destroy', as: 'logout'
   get 'admin_menu' => 'welcome#admin_menu' 
-  
+  get 'late_entries' => 'entries#late_entries'  
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:edit]
 
