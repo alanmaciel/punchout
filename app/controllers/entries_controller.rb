43e4186fda2d9a8504493639f8ceaf14fc8fedea
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-09-08 10:37:33 -0500

    Added Employees with adsences feature

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,8 +3,22 @@ class EntriesController < ApplicationController
 
   def late_entries
     @current_period = Period.current_period
+    @initial_date = @current_period.initial_date
+    @final_date = @current_period.final_date
+
     @arrival_tolerance = Schedule.first.arrival_tolerance.strftime("%H:%M")
-    @late_entries = Entry.late_entries(@arrival_tolerance) 
+    @late_entries = Entry.late_entries(@arrival_tolerance, @initial_date, @final_date) 
+  end
+
+  def employees_absences
+    @current_period = Period.current_period
+    @initial_date = @current_period.initial_date
+    @final_date = @current_period.final_date
+    
+    entries = Entry.current_period_entries(@initial_date, @final_date)
+    days = Entry.get_period_working_days(@initial_date, @final_date)
+    employees = Entry.extract_employees_list(entries)
+    @absences = Entry.get_absences(employees, days, entries)
   end
 
 end
