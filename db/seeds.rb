26581ba19f195c4923f3205ad5aa3846860c92e4
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-11-04 13:41:35 -0600

    Changed seed to generate dates dynamically

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,82 +1,101 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+# First create the admin user, this user is used by the HR department to get
+# the reports in the 'Admin Menu', 'Employees arrving late' and 'Employees with
+# absences'.
 User.create([{  name: 'Admin',
                 email: 'admin@example.com',
                 password: '1234567890',
                 password_confirmation: '1234567890'
              }])
 
-Entry.create([{
-                barcode: '00001',
-                arrival_at: '2016-09-12 09:00:00',
-                departure_at: '2016-09-12 17:00:00'},
-              { barcode: '00002',
-                arrival_at: '2016-09-12 08:54:12',
-                departure_at: '2016-09-12 18:10:12'},
-              { barcode: '00003',
-                arrival_at: '2016-09-12 09:15:00',
-                departure_at: '2016-09-12 09:10:00'},
-              { barcode: '00004',
-                arrival_at: '2016-09-12 09:08:00',
-                departure_at: '2016-09-12 16:49:55'},
-              { barcode: '00005',
-                arrival_at: '2016-09-12 09:03:09',
-                departure_at: '2016-09-12 17:45:32'},
-              { barcode: '00006',
-                arrival_at: '2016-09-12 08:20:23',
-                departure_at: '2016-09-12 16:30:34'},
-              { barcode: '00007',
-                arrival_at: '2016-09-12 09:11:12',
-                departure_at: '2016-09-12 17:59:48'},
-              { barcode: '00008',
-                arrival_at: '2016-09-12 09:18:12',
-                departure_at: '2016-09-12 18:23:22'},
-              { barcode: '00009',
-                arrival_at: '2016-09-12 09:20:28',
-                departure_at: '2016-09-12 19:23:22'},
-              { barcode: '00010',
-                arrival_at: '2016-09-12 10:15:23',
-                departure_at: '2016-09-12 18:00:09'},
-              { barcode: '00001',
-                arrival_at: '2016-09-30 10:12:21',
-                departure_at: '2016-09-30 18:12:22'},
-              { barcode: '00002',
-                arrival_at: '2016-09-30 9:02:11',
-                departure_at: '2016-09-30 17:23:12'},
-              { barcode: '00003',
-                arrival_at: '2016-09-30 09:15:23',
-                departure_at: '2016-09-30 18:22:11'}
-             ] )
+# Second create three periods, the HR department using the admin user must create
+# the periods, if they work by week, then they need to create four periods each
+# month, if they work by fortnight then they need to create two periods for
+# month. if they pay monthly then they only need to create twelve periods per
+# year.
+
+# crear los periodos para probar 6 meses atras y 6 meses en el futuro
+# por quincena:
+#
+# crear periodo del mes pasado al dia de hoy
+# crear periodo del mes actual al dia de hoy
+# creat periodo del mes siguiente al actual al dia de hoy
+
+# last month first day
+# last month end day
+
+# this month first day
+# this month end day
+
 
 Period.create([{
-                 initial_date: '2016-08-16',
-                 final_date: '2016-08-31',
-                 payday: '2016-08-31',
-                 description: "August's 2nd fortnight"
+                 initial_date: 1.month.ago.beginning_of_month.strftime("%Y-%m-%d"),
+                 final_date: 1.month.ago.end_of_month.strftime("%Y-%m-%d"),
+                 payday: 1.month.ago.end_of_month.strftime("%Y-%m-%d"),
+                 description: 1.month.ago.strftime("%B %Y")
                },
                {
-                 initial_date: '2016-09-01',
-                 final_date: '2016-09-15',
-                 payday: '2016-09-15',
-                 description: "September's 1st fortnight"
-               },
-               {
-                 initial_date: '2016-09-16',
-                 final_date: '2016-09-30',
-                 payday: '2016-09-30',
-                 description: "September's 2nd fortnight"
-               },
+                 initial_date: Time.now.beginning_of_month.strftime("%Y-%m-%d"),
+                 final_date: Time.now.end_of_month.strftime("%Y-%m-%d"),
+                 payday: (Time.now + 3.days).strftime("%Y-%m-%d"),
+                 description: Time.now.strftime("%B %Y")
+               }])
 
-              ])
+# Third we create the arrival tolerance and the working hours: Check in time
+# and check out time:
 
 Schedule.create([{
                    work_starts_at: '09:00:00',
-                   work_ends_at: '18:00:00',
+                   work_ends_at: '17:00:00',
                    arrival_tolerance: '09:10:00'
-                 }
-                ])
+                 }])
+
+# Finally We need some employees entries:
+# Arriving on time:
+# Arriving late:
+
+# Crear entradas para 5 empleados hipotéticos de la empresa con las
+# tarjetas con el codigo de barras:
+# 00001 hasta 00004
+# Ejemplo de formato de fecha y hora: '2016-09-12 09:00:00'
+#
+# Quincena pasada
+# 00001 siempre llega a tiempo 9:00 en punto 
+# 00002 llega 1 minuto tarde 9:01
+# 00003 siempre llega tarde 11 minutos tarde 9:11 (fuera de la tolerancia)
+# 00004 esta ausente (no hay registro)
+# 00005 en adelante llegan 10 minutos antes 8:50
+Entry.create([
+               { barcode: '00001',
+                 arrival_at: 15.days.ago.strftime("%Y-%m-%d") + ' 09:00:00',
+                 departure_at: 15.days.ago.strftime("%Y-%m-%d")+ ' 17:00:00'},
+               { barcode: '00002',
+                 arrival_at: 15.days.ago.strftime("%Y-%m-%d") + ' 09:01:00',
+                 departure_at: 15.days.ago.strftime("%Y-%m-%d")+ ' 17:00:00'},
+               { barcode: '00003',
+                 arrival_at: 15.days.ago.strftime("%Y-%m-%d") + ' 09:11:00',
+                 departure_at: 15.days.ago.strftime("%Y-%m-%d")+ ' 17:00:00'},
+               { barcode: '00005',
+                 arrival_at: 15.days.ago.strftime("%Y-%m-%d") + ' 08:50:00',
+                 departure_at: 15.days.ago.strftime("%Y-%m-%d")+ ' 17:00:00'}
+             ])
+# Quincena actual
+# 00001 llega a tiempo 9:00 en punto 
+# 00002 llega 1 minuto tarde 9:01
+# 00003 llega 11 minutos tarde 9:11 (fuera de la tolerancia)
+# 00004 esta ausente (no hay registro)
+# 00005 llega 10 minutos antes 8:50
+Entry.create([
+               { barcode: '00001',
+                 arrival_at: Time.now.strftime("%Y-%m-%d") + ' 09:00:00',
+                 departure_at: Time.now.strftime("%Y-%m-%d")+ ' 17:00:00'},
+               { barcode: '00002',
+                 arrival_at: Time.now.strftime("%Y-%m-%d") + ' 09:01:00',
+                 departure_at: Time.now.strftime("%Y-%m-%d")+ ' 17:00:00'},
+               { barcode: '00003',
+                 arrival_at: Time.now.strftime("%Y-%m-%d") + ' 09:11:00',
+                 departure_at: Time.now.strftime("%Y-%m-%d")+ ' 17:00:00'},
+               { barcode: '00005',
+                 arrival_at: Time.now.strftime("%Y-%m-%d") + ' 08:50:00',
+                 departure_at: Time.now.strftime("%Y-%m-%d")+ ' 17:00:00'}
+             ])
